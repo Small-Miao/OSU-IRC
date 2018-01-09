d1573cf9e1c40ce51a84d4e3cb4dcfde58ec5b5e
Small_Miao <1773805744@qq.com>
2018-01-09 16:49:43 +0800

    添加项目文件。

---
+++ OSU-IRC/Irc.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Meebey.SmartIrc4net;
+
+namespace OSU_IRC聊天
+{
+    class Irc
+    {
+        public static IrcClient IRC = new IrcClient();
+        private static Thread _ListenThread;
+        Program program = new Program();
+        public string chan { set { chanl = value; } get { return chanl; } }
+        public string chanl;
+        public string User{set{ IrcUser = value; } get{ return IrcUser; } }
+        public string IrcUser;
+        public string Password { set{ IrcPassword = value; }get{ return IrcPassword; } }
+        string IrcPassword;
+        public void Disco()
+        {
+            IRC.Disconnect();
+        }
+        public Irc()
+        {
+
+
+
+
+            IRC.OnChannelMessage += IRC_OnChannelMessage;
+            IRC.OnQueryAction += IRC_OnQueryAction;
+            IRC.OnQueryMessage += IRC_OnQueryMessage;
+
+           
+
+
+
+        }
+        public  void IRCEncoding()
+        {
+            IRC.Encoding = Encoding.UTF8;
+        }
+        public void Connect()
+        {
+            IRC.Connect("irc.ppy.sh", 6667);
+
+        }
+        public void IrcLogin()
+        {
+                string id = IrcUser;
+            try
+            {
+                IRC.Login(id, id, 0, id, IrcPassword); 
+            }
+            catch 
+            {
+
+                Console.WriteLine( "IrcLogin Error");
+            }
+            
+           
+        }
+        public void Start()
+        {
+            _ListenThread = new Thread(new ThreadStart(IRCThread));
+            _ListenThread.Start();
+        }
+        public void Join()
+        {
+            IRC.RfcJoin(chanl);
+            _ListenThread = new Thread(new ThreadStart(IRCThread));
+
+            try
+            {
+                _ListenThread.Start();
+            }
+            catch 
+            {
+
+                
+            } 
+        }
+        private void IRC_OnQueryMessage(object sender, IrcEventArgs e)
+        {
+
+            Console.WriteLine("[" + System.DateTime.Now + "私聊" + "]" + e.Data.Nick+":" + e.Data.Message);
+
+        }
+
+        public void ExitChanl()
+        {
+            IRC.RfcQuit(chanl);
+        }
+        private void IRC_OnQueryAction(object sender, ActionEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+        private void IRC_OnChannelMessage(object sender, IrcEventArgs e)
+        {
+
+        
+            Console.WriteLine("[" + System.DateTime.Now + "来自" + e.Data.Channel + "]" + e.Data.Nick + ":" + e.Data.Message);
+           
+
+        }
+
+        public static void send(string message, string Chanl)
+        {
+            
+            IRC.SendMessage(SendType.Message,Chanl, message);
+        }
+        public   void IRCsend(string Msg,string id)
+        {
+            IRC.SendMessage(SendType.Message,id,Msg);
+        }
+        public void IRCThread()
+        {
+            try
+
+            {
+                IRC.Listen();
+            }
+            catch { }
+        }
+    }
+}
---
+++ OSU-IRC/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSU_IRC聊天
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool a = true;
+            Irc Irc = new Irc();
+            string User;
+            string Password;
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Loding 成功");
+            A: try
+            {
+                Irc.IRCEncoding();
+                Console.WriteLine("编码设置成功");
+            }
+            catch
+            {
+                Console.WriteLine("编码设置失败");
+
+            }
+            try
+            {
+                Irc.Connect();
+                Console.WriteLine("服务器链接成功 IP:" + "irc.ppy.sh:6667");
+            }
+            catch
+            {
+
+                Console.WriteLine("链接失败");
+            }
+
+            while (a)
+            {
+                Console.WriteLine("设置用户名");
+                User = Console.ReadLine();
+                Irc.IrcUser = User;
+                Console.WriteLine("输入密码");
+                Password = Console.ReadLine();
+                Irc.Password = Password;
+                a = false;
+
+            }
+            Console.WriteLine("登录服务器ING");
+            try
+            {
+                Irc.IrcLogin();
+                Console.WriteLine("登录成功");
+            }
+            catch
+            {
+
+                Console.WriteLine("登录失败");
+            }
+            Console.WriteLine("使用模式：1.频道 2.私聊");
+            int choese = 0;
+            try
+            {
+                choese = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+
+
+            }
+
+            switch (choese)
+            {
+                case 1:
+                    Console.WriteLine("输入进入频道 无需输入井号 如为MP房间 请输入 mp_房间号");
+                    string OSUIRC;
+                    string OSUMsg;
+                    OSUIRC = Console.ReadLine();
+                    Irc.chan = "#" + OSUIRC;
+                    Irc.Join();
+                    while (true)
+                    {
+
+                        OSUMsg = Console.ReadLine();
+                        if (OSUMsg == "Exit Msg")
+                        {
+                            Irc.ExitChanl();
+                            Irc.Disco();
+                            Console.WriteLine("已经退出频道");
+                            break;
+                        }
+                        Irc.send(OSUMsg, Irc.chanl);
+                    }
+                    goto A;
+
+                case 2:
+                    Console.WriteLine("输入私聊人ID");
+                    string ID;
+                    ID = Console.ReadLine();
+                    Irc.Start();
+                    Console.WriteLine("输入消息");
+                    string Msg;
+                    while (true)
+                    {
+                        Msg = Console.ReadLine();
+                        if (Msg == "Exit Msg")
+                        {
+                            Console.WriteLine("退出私聊");
+
+                            Irc.Disco();
+                            break;
+                        }
+                        Irc.IRCsend(Msg, ID);
+                    }
+                    goto A;
+
+
+                default:
+                    Console.WriteLine("帮助:Exit Msg即可退出聊天 私聊信息是会一直接收的 频道信息 只可以在使用频道聊天时接收");
+                    goto A;
+            }
+
+        }
+    }
+}
